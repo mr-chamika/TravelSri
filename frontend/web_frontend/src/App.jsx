f2348564404abdc321647ee6dc12d6bcbafbdf74
mr-chamika <chamikauni2001@gmail.com>
2025-06-05 12:54:49 +0530

    web setup

--- frontend/web_frontend/src/App.jsx
+++ frontend/web_frontend/src/App.jsx
@@ -1,93 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+//import './App.css';
+import Login from './pages/Login/Login';
 
 function App() {
-  const [name, setName] = useState('')
-  const [address, setAddress] = useState('')
-  const [students, setStudents] = useState([])
-  const [ok, setOk] = useState(false)
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-
-    fetch('http://localhost:8080/student/add', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name: name, address: address }),
-    })
-      .then(res => res.text())
-      .then(data => { console.log(data) })
-      .catch(err => console.error('Error:', err))
-  }
-
-  const getAll = () => {
-
-    setOk(true)
-
-    fetch('http://localhost:8080/student/getAll')
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-        setStudents(data)
-      })
-      .catch(err => console.error('Error:', err))
-
-  }
-
-  const less = () => {
-
-    setOk(false);
-
-  }
-
   return (
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-        <div>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            placeholder="Enter name"
-            required
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-            placeholder="Enter address"
-            required
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-      <button onClick={getAll}>Get all</button>
-      {ok &&
+    <BrowserRouter>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          {/* <Route path="/login" element={<Login />} /> */}
 
-        <div>
-          {
-            students.map((student) => {
-
-              return (
-                <div key={student._id} style={{ marginBottom: '20px' }}>
-                  <p>Name: {student.name}</p>
-                  <p>Address: {student.address}</p>
-                </div>
-              )
-
-            })
-          }
-          <button onClick={less}>show less</button>
-        </div>
-
-      }
-
-    </div>
-  )
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
